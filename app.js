323afcd9cecf3bd02e21f1008a82c5004d039f46
Conor Martin <conorarranmore@gmail.com>
2018-06-03 15:45:38 +0100

    changed port

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(8000, function() {
+app.listen(process.env.PORT, function() {
     console.log("YelpCamp Server has Started!");
 });
